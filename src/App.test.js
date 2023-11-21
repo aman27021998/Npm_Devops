8d23b3a14c0dbec6e00a1849453a2008112081e2
aman27021998 <90708040+aman27021998@users.noreply.github.com>
2023-11-21 15:05:11 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//          debug();
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+// App.test.js
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
-test('renders learn react link', () => {
+ 
+test('renders Hello World text', () => {
   render(<App />);
-         debug();
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const helloWorldElement = screen.getByText(/Hello World/i);
+  expect(helloWorldElement).toBeInTheDocument();
 });
