4968b2d64665ff64698ae943101d777aa91fbf9e
aman27021998 <90708040+aman27021998@users.noreply.github.com>
2023-11-21 14:59:51 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByRole('link', { name: /learn react/i });
+         debud();
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
